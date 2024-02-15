ee155dafbb6d1d32b1fbf5c176cf05f565096d09
LoDestino <raylight018@icloud.com>
2024-02-15 18:48:01 -0500

    yeah

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ class Window(ctk.CTk):
         self.TextFrameWidth = 475
         self.TextFrameHeight = 450
 
-        # below is the left side bar, with pack_propagate is being used to keep its own size and not surround shit
+        # below is the left sidebar, with pack_propagate is being used to keep its own size and not surround shit
         self.HomeBar = ctk.CTkFrame(self, height=450)
         self.HomeBar.pack_propagate(False)
         self.HomeBar.pack(side="left")
 
         self.label = ctk.CTkLabel(self.HomeBar, text="Text Editor", font=("calibri", 30, "bold"))
-        self.label.pack(side="top")
+        self.label.pack(side="top", pady=10)
+
+        self.WriteButton = ctk.CTkButton(self.HomeBar, text="Write", command=self.Write)
+        self.WriteButton.pack(side="bottom", pady=20)
 
         # The frame contains the text Content and other stuff
         self.ContentFrame = ctk.CTkScrollableFrame(self,height=self.TextFrameHeight, width=self.TextFrameWidth)
@@ -28,7 +31,13 @@ class Window(ctk.CTk):
 
     # the function below is activated when the Save button is pressed.
     def Write(self):
-        pass
+        self.TextContent = str(self.entry.get(1.0, "end"))
+        self.DefaultFileType = ".txt"
+        if self.TextContent.find("#include") != -1:
+            self.DefaultFileType = ".cpp"
+
+        with open("file"+self.DefaultFileType, "w") as file:
+            file.write(self.TextContent)
 
 window = Window()
 window.mainloop()
