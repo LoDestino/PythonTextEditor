3bcb4a91c9db4e2211842633b3bc74d7fcee213f
LoDestino <raylight018@icloud.com>
2024-02-15 18:21:28 -0500

    yeah

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Window(ctk.CTk):
 
         self.entry = ctk.CTkTextbox(self.ContentFrame, height=self.TextFrameHeight, width=self.TextFrameWidth)
         self.entry.pack(side="left")
-    # the function below is activated when the Save button is pressed
+    # the function below is activated when the Save button is pressed.
     def Write(self):
         pass
 
