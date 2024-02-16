33060a0f01e1e847ad2564a6ce2082092f435589
LoDestino <raylight018@icloud.com>
2024-02-15 21:49:35 -0500

    yeah

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import tkinter as tk
 import customtkinter as ctk
 from tkinter import filedialog
 
-
 class Window(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         self.geometry("650x450")
         self.title("TXT EDITOR")
+
+        self.Counter = 0
 
         self.TextFrameWidth = 475
         self.TextFrameHeight = 450
@@ -25,23 +26,67 @@ class Window(ctk.CTk):
         self.WriteButton.pack(side="bottom", pady=20)
 
         self.OpenButton = ctk.CTkButton(self.HomeBar, text="Open", command=self.Open)
-        self.OpenButton.pack(side="bottom", pady=25)
+        self.OpenButton.pack(side="bottom", pady=20)
         # The frame contains the text Content and other stuff
-        self.ContentFrame = ctk.CTkScrollableFrame(self,height=self.TextFrameHeight, width=self.TextFrameWidth)
+        self.ContentFrame = ctk.CTkTabview(self,height=self.TextFrameHeight, width=self.TextFrameWidth)
+        self.ContentFrame.pack_propagate(False)
         self.ContentFrame.pack(side="right")
 
-        self.entry = ctk.CTkTextbox(self.ContentFrame, height=self.TextFrameHeight, width=self.TextFrameWidth)
-        self.entry.pack(side="left")
+        self.FileNewButton = ctk.CTkButton(self.HomeBar, text="New File", command=self.TabSystem)
+        self.FileNewButton.pack(side="bottom", pady=20)
 
     # the function below is activated when the Save button is pressed.
+
+    def TabSystem(self):
+        def e():
+            self.TabFileName = str(self.promptentry.get())
+            self.prompt.destroy()
+
+        self.Counter += 1
+        self.TabFileName = None
+        self.new_tab = self.ContentFrame.add("Tab " + str(self.Counter))
+
+        self.ContentFrame.set("Tab " + str(self.Counter))
+
+        # prompt instantly disappears?
+
+        #self.open_windows = []
+
+        #self.open_windows.append(self.prompt)
+
+
+        self.entry = ctk.CTkTextbox(self.new_tab, width=self.TextFrameWidth, height=self.TextFrameHeight)
+        self.entry.pack(side="left")
+
+        self.prompt = ctk.CTkToplevel(self)
+        self.prompt.geometry("350x150")
+        self.prompt.transient(self)
+
+        self.promptframe = ctk.CTkFrame(self.prompt, fg_color="transparent")
+        self.promptframe.pack()
+
+        self.promptlabel = ctk.CTkLabel(self.promptframe, text="New file's name: ")
+
+        self.promptentry = ctk.CTkEntry(self.promptframe)
+
+        self.promptbutton = ctk.CTkButton(self.prompt, text="Next", command=e)
+
+        self.promptlabel.pack(side="left", pady=30)
+        self.promptentry.pack(side="left", padx=10)
+        self.promptbutton.pack(side="bottom", pady=10, padx=10)
+
     def Write(self):
-        path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
-        if path:
-            with open(path, "w") as file:
-                file.write(self.entry.get("1.0", ctk.END))
+        self.CurrentFile = self.ContentFrame.get()
+        self.FileName = self.TabFileName
+        self.CurrentFileContent = self.entry.get(1.0, ctk.END)
+        self.FileType = ".txt"
+        if self.CurrentFileContent.find("#include") != -1:
+            self.FileType = ".cpp"
+        with open(self.FileName+self.FileType, "w") as file:
+            file.write(self.CurrentFileContent)
     
     def Open(self):
-        path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+        path = filedialog.askopenfilename(filetypes=[("All Files", "*")])
         if path:
             with open(path, "r") as file:
                 self.entry.delete("1.0", ctk.END)
